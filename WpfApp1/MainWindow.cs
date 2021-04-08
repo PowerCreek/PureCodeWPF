cf06ce3bd7042a170af092b6cc81b6584d38c0ab
EpicMice <micecd@gmail.com>
2021-04-07 21:39:28 -0500

    .

---
+++ WpfApp1/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Application
+{
+
+    [STAThread]
+    public static void Main()
+    {
+        var app = new MainWindow();
+        Window root = new Window();
+        root.Title = "Title";
+
+        root.Content = "test content";
+        root.Show();
+        app.MainWindow = root;
+        app.Run();
+    }
+}
